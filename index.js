4940593266f9ad4b8cb891b66eb445579df225ec
imagicdev <95496703+imagicdev@users.noreply.github.com>
2022-05-02 12:35:07 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var mobile = false
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    window.navigate.vibrate([300, 50, 300, 50, 300]);
+    alert("Welcome to Encryped Messaging Center mobile!");
+	mobile = true
+}else{
+	alert("Welcome to Encryped Messaging Center);
+}
+		  
