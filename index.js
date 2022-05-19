ffac96c4fb40152a6e6c61d96a065355105139cf
imagicdev <95496703+imagicdev@users.noreply.github.com>
2022-05-18 19:20:04 -0600

    Update
    Changed if statement to more reliable state in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mobile = false
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    window.navigate.vibrate([300, 50, 300, 50, 300]);
-    alert("Welcome to Encryped Messaging Center mobile!");
+if(navigator.userAgent.toLowerCase().match(/mobile/i)) {
+	window.navigate.vibrate([300, 50, 300, 50, 300]);
+	alert("Welcome to Encryped Messaging Center mobile!");
 	mobile = true
 }else{
 	alert("Welcome to Encryped Messaging Center);
